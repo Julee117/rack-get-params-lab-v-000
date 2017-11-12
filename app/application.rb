00b9aac127579aeca85648344d0e16c19761ebf9
Julianne Kim <juliannne@gmail.com>
2017-11-12 04:56:11 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -26,6 +26,7 @@ class Application
   def app(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+    
     if req.path.match(/cart/)
       @@cart.each do |cart_item|
         resp.write "#{cart_item}\n"
