b1111f8a4f29410794ea6c84751084ad77de5502
Julianne Kim <juliannne@gmail.com>
2017-11-12 05:12:30 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,7 @@ class Application
       @@cart.each do |cart_item|
         resp.write "#{cart_item}\n"
       end
-    elsif !req.path.match(/cart/)
+    elsif @@cart.empty?
       resp.write "Your cart is empty"
     elsif req.path.match(/search/)
       search_term = req.params["q"]
